542dcc2d99f0b41cc1c7fb567a3d54fae425a662
HarukawaTakiko <adawang3dsmax@gmail.com>
2017-03-03 22:27:39 +0800

    新增POKEMO類別

--- CS280-LECTUREL-Examples/CS280-LECTUREL-Examples/Program.cs
+++ CS280-LECTUREL-Examples/CS280-LECTUREL-Examples/Program.cs
@@ -10,7 +10,14 @@ namespace CS280_LECTUREL_Examples
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            pokemon pokemon = new pokemon();
+            pokemon.Height = 2.0f;
+            pokemon.Weight = 12.7f;
+            pokemon.Category = "Lizard";
+            pokemon.Abilities = "Blaze";
+            pokemon.Gender = "M";
+            Console.WriteLine("身高:{0},體重{1}",
+                pokemon.Height, pokemon.Weight);)
         }
     }
 }
